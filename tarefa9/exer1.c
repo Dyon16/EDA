aacd0034e3ae655de2117cabdcc40da745b6e8ab
Dyon16 <jardel.dyonisio@hotmail.com>
2020-07-22 19:33:51 -0300

    Adicionando explicação de parametros do fwrite

--- tarefa9/exer1.c
+++ tarefa9/exer1.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/*
+- o i= é a variável que vais gravar no arquivo "arq"
+- o sizeof é o tamanho do tipo que será gravado
+- 10 é a quantidade de registros, como "i" é um vetor de 10 registros, tu gravarás ele todo de uma vez só no arquivo
+- arq é a varíável que contém o nome do arquivo e o tipo de operação que farás nele (teste.dat e wb)
+*/
+
 typedef struct
 {
     char nome[20];
